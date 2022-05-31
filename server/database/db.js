d08f75f1db528838acf325af56d618007b2cbdda
Gaurab Shrestha <gaurab.stha.5@gmail.com>
2022-05-31 13:33:12 +0545

    Second Push

--- server/database/db.js
+++ server/database/db.js
@@ -1,10 +1,14 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "admin123",
-  database: "farmer_friend",
+  // host: "localhost",
+  // user: "root",
+  // password: "admin123",
+  // database: "farmer_friend",
+  host: "us-cdbr-east-05.cleardb.net",
+  user: "be4fd9448086e0",
+  password: "99e4f13d",
+  database: "heroku_2f3a5afed2151cd",
 });
 db.connect((err) => {
   if (err) {
