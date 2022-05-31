598ba7f4ed053f1ca497eb2969b8b8a2c45ba84e
Gaurab Shrestha <gaurab.stha.5@gmail.com>
2022-05-31 14:06:38 +0545

    Changed the port

--- server/index.js
+++ server/index.js
@@ -27,6 +27,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Server is running on port 5000.");
 });
